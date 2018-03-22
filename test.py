a58ab49a4dcf4652f0c51595548f7a2550562f95
lacow123 <37667093+lacow123@users.noreply.github.com>
2018-03-22 12:18:55 -0400

    Initial commit

---
+++ test.py
@@ -0,0 +1,16 @@
+age = int(input("What is your age ")) #Ask what my age is
+if age < 16: #verifies if i am over 16 if not prints
+    print("You must be 16 and older.")
+elif age >= 16: #if I am over 16 prints
+    print("You are older than 16 you may continue")
+    name = (input("What is your name? ")) # ask for my age
+    print('Hello how are you ' + str(name) ,'You are ' + str(age) + ' years old')
+height = (input("what is your height"))
+if height > 55:
+    print('You are very tiny')
+elif height < 55:
+    print('You are very tall')
+
+
+
+
